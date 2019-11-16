41a651516f357b814288f1587f36c8b220c64049
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-16 19:26:48 +0530

    usercase5: changes need to update

--- tictactoe.sh
+++ tictactoe.sh
@@ -7,6 +7,8 @@ HUMAN=0
 human_Symbol=0
 computer_Symbol=0
 count=1
+row1=0
+countnumber=0
 
 declare -a matrixBoard
 function resetBoard()
@@ -32,23 +34,74 @@ function check_For_Symbol()
 
 }
 
+function rowwinning()
+{
+        var=$1
+        row1=$(( $row1+1 ))
+	if [ matrixBoard["$row1"]==$var && matrixBoard["$row1+1"]==$var] || [matrixBoard["$row1"]==$var && matrixBoard["$row1+2"]==$var ]
+        then
+        countnumber=$(( $countnumber+1 ))
+        else
+        row1=$(( $row1+3 ))
+
+        if [ $countnumber -eq 1 ]
+        then
+            echo "Sucess"
+        fi
+
+        fi
+
+}
+
+function columnwinning()
+{
+      var=$1
+      
+	if [ matrixBoard["$col"]==$var && matrix]
+        then
+            count= $(( $count+1 ))
+        if [ $count -eq 3 ]
+        then
+            echo "Sucess"
+        fi
+	
+	fi
+}
+
 function head_Tail()
 {
   	toss=$((RANDOM%2))
   	if [ $toss -eq $HUMAN ]
     	then
 		echo "Your Move"
-                printBoard
-                var=$(check_For_Symbol)
-                echo $var
-                read -p "Entered the position u want to enter:" move
-		matrixBoard["$move"]=$var
-                printBoard
+                play               
  	else
   		echo "computer Move"
                 printBoard
 
         fi
+
+}
+function play()
+{
+                while [ true  ]
+		do
+                  var=$( check_For_Symbol )
+		for((count=1;count<=2;count++))
+                do
+			if [ $count -eq 1 ]
+			then	
+			printBoard
+	                echo $var
+	                read -p "Entered the position u want to enter:" move
+			matrixBoard["$move"]=$var
+	                printBoard
+                        rowwinning $var
+			else
+			play		
+			fi
+		done
+		done
 
 }
 
