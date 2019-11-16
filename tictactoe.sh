16d2d4f528866084c4576d359b7250802d8a86fc
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-16 17:21:00 +0530

    uercase3:checked for first Move for human or computer

--- tictactoe.sh
+++ tictactoe.sh
@@ -25,15 +25,31 @@ function check_For_Symbol()
         fi
 
 }
+
+function head_Tail()
+{
+  	toss=$((RANDOM%2))
+  	if [ $toss -eq $HUMAN ]
+    	then
+		echo "First Move"
+ 	else
+  		echo "Second Move"
+        fi
+
+}
+
 function printBoard()
 { 
+
  	echo   "| "${matrixBoard[1]}" | "${matrixBoard[2]}" | "${matrixBoard[3]}" |"
 
  	echo   "| "${matrixBoard[4]}" | "${matrixBoard[5]}" | "${matrixBoard[6]}" |"
 
  	echo   "| "${matrixBoard[7]}" | "${matrixBoard[8]}" | "${matrixBoard[9]}" |"
+
 }
 
 resetBoard
 printBoard
 check_For_Symbol
+head_Tail
