b27799cf3134372a1a389bd382509ec0135ee006
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-16 16:44:00 +0530

    usercase1:reseted the board and printed it

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,2 +1,21 @@
-#Welcome to tic tac toe
+#!/bin/bash -x
 
+declare -a matrixBoard
+count=1
+function resetBoard()
+{
+for (( row=1;row<=9;row++ ))
+do
+  matrixBoard[$row]=$count
+  count=$(( $count+1 ))
+done
+}
+function printBoard()
+{ 
+ echo   "|"${matrixBoard[1]}"|"${matrixBoard[2]}"|"${matrixBoard[3]}"|"
+ echo   "|"${matrixBoard[4]}"|"${matrixBoard[5]}"|"${matrixBoard[6]}"|"
+ echo   "|"${matrixBoard[7]}"|"${matrixBoard[8]}"|"${matrixBoard[9]}"|"
+}
+
+resetBoard
+printBoard
